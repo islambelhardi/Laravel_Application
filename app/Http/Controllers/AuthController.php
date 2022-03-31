808fe95d3eb6e44ce69fea11ab2c3b8937058c28
islambelhardi <86858341+islambelhardi@users.noreply.github.com>
2022-03-31 15:35:40 +0100

    signup added and login but without access token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,23 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    public function create(){
+        return view('SuccessfulEmailVerification');
+    }
+    public function register(Request $request){
+        $rules = [
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users',
+            'password' => 'required|string|min:8'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        event(new Registered($user));
+        Auth::login($user);
+        return Response('signup succes please verify your email', 200);
+    }
     public function login(Request $request)
     {
         $user = User::where('email', $request->email)->first();
+        // if user is correct and has verified email
         if ($user &&
             Hash::check($request->password, $user->password) && $user->hasVerifiedEmail()) {
             return $user;
         }
+        // if user is correct and hasn't verified email
         if ($user &&
             Hash::check($request->password, $user->password) && ! $user->hasVerifiedEmail()) {
-            return Response('email need to verify', 401);
+            //return Response('email need to verify', 401);
+            return response()->json('email need to verify',401);
         }
-        return Response('Your email or password incorrect', 400);
+        // if user coordinate are wrong
+        //return Response('Your email or password incorrect', 400);
+        return response()->json('Your email or password incorrect',400);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -14,8 +17,24 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+    //to make a sign up
+    Route::post('register',[AuthController::class,'register']);
+    //login view
+    Route::get('login',[AuthController::class,'create'])->name('login');
+    //to make a login
+    Route::post('login',[AuthController::class,'login'] );
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::post('login',[AuthController::class,'login'] );
+
+    Route::get('/email/verify', function () {
+        return Response('Email verification sent', 200);
+    })->middleware('auth')->name('verification.notice');
+// the route that handle email verification
+    Route::get('/email/verify/{id}/{hash}', function (Request $request) {
+        $user = User::where('id', $request->id)->first();
+        if (! $user->hasVerifiedEmail()) {
+            $user->markEmailAsVerified();
+            event(new Verified($user));
+        }
+        echo 'email verified';
+    })->middleware(['signed'])->name('verification.verify');
+
